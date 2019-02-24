fda57a2b2cb1dfc530bc8727ec1c04b699be0f65
拉格朗日无悔 <2545197103@qq.com>
2019-02-24 15:56:43 +0800

    dnomovie

---
+++ dnomovie/dnomovie/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+#This will make sure the app is always imported when
+#Django starts so that shared_taks will use this app
+from .celery import app as celery_app
+
+import pymysql
+pymysql.install_as_MySQLdb()
